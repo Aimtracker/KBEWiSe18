b89bd42bd3f7c0a400c393bbedc195c2e94d1482
Dominik Wegner <dominik.wegner@navitas.de>
2018-12-16 19:47:05 +0100

    Tests work smoothly now

--- songsrx/src/main/java/de/htw/ai/kbe/songsrx/service/SongWebService.java
+++ songsrx/src/main/java/de/htw/ai/kbe/songsrx/service/SongWebService.java
@@ -15,68 +15,72 @@ import java.util.List;
 @Path("songs")
 public class SongWebService {
 
-        @Context
-        UriInfo uriInfo;
+    @Context
+    UriInfo uriInfo;
 
-        @Inject
-        ISongList songList;
+    @Inject
+    ISongList songList;
 
-        @GET
-        @Secured
-        @Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
-        public List<Song> getSongs() {
-            return songList.getAll();
+    @GET
+    @Secured
+    @Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
+    public List<Song> getSongs() {
+        return songList.getAll();
+    }
+
+    @GET
+    @Secured
+    @Path("/{id}")
+    @Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
+    public Response getSong(@PathParam("id") Integer id) {
+        try {
+            Song song = songList.getById(id);
+            return Response.ok(song).build();
+        } catch (NotFoundException e) {
+            return Response.status(Response.Status.NOT_FOUND).build();
+        }
+    }
+
+    @POST
+    @Secured
+    @Consumes({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
+    @Produces(MediaType.TEXT_PLAIN)
+    public Response createSong(@Valid Song song) {
+        Integer id = songList.add(song);
+        //return Response.ok("Song added (new id: " + id + ")").build();
+        UriBuilder uriBuilder = uriInfo.getAbsolutePathBuilder();
+        uriBuilder.path(id.toString());
+        return Response.created(uriBuilder.build()).entity("Song added (new id: " + id + ")").build();
+    }
+
+    @PUT
+    @Secured
+    @Consumes({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
+    @Path("/{id}")
+    public Response updateSong(@PathParam("id") Integer id, @Valid Song song){
+        if (song.getId() != null && id != song.getId()) {
+            throw new BadRequestException("Something is not right here fam");
         }
 
-        @GET
-        @Secured
-        @Path("/{id}")
-        @Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
-        public Response getSong(@PathParam("id") Integer id){
-                try {
-                        Song song = songList.getById(id);
-                        return Response.ok(song).build();
-                }catch(NotFoundException e){
-                        return Response.status(Response.Status.NOT_FOUND).build();
-                }
+        try {
+            song.setId(id);
+            songList.update(song);
+            return Response.status(Response.Status.NO_CONTENT).build();
+        } catch (NotFoundException e) {
+            return Response.status(Response.Status.NOT_FOUND).build();
+        }
+    }
+
+    @DELETE
+    @Secured
+    @Path("/{id}")
+    public Response deleteSong(@PathParam("id") Integer id) {
+        try {
+            songList.delete(id);
+            return Response.status(Response.Status.NO_CONTENT).build();
+        } catch (NotFoundException e) {
+            return Response.status(Response.Status.NOT_FOUND).build();
         }
 
-        @POST
-        @Secured
-        @Consumes({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
-        @Produces(MediaType.TEXT_PLAIN)
-        public Response createSong(@Valid Song song){
-                Integer id = songList.add(song);
-                //return Response.ok("Song added (new id: " + id + ")").build();
-                UriBuilder uriBuilder = uriInfo.getAbsolutePathBuilder();
-                uriBuilder.path(id.toString());
-                return Response.created(uriBuilder.build()).entity("Song added (new id: " + id + ")").build();
-        }
-
-        @PUT
-        @Secured
-        @Consumes({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
-        @Path("/{id}")
-        public Response updateSong(@PathParam("id") Integer id, @Valid Song song) throws NotFoundException {
-                if(song.getId() != null && id != song.getId()){
-                        throw new BadRequestException("Something is not right here fam");
-                }
-
-                song.setId(id);
-                songList.update(song);
-                return Response.status(Response.Status.NO_CONTENT).build();
-        }
-
-        @DELETE
-        @Secured
-        @Path("/{id}")
-        public Response deleteSong(@PathParam("id") Integer id){
-                try{
-                        songList.delete(id);
-                        return Response.status(Response.Status.NO_CONTENT).build();
-                }catch(NotFoundException e){
-                        return Response.status(Response.Status.NOT_FOUND).build();
-                }
-
-        }
+    }
 }
--- songsrx/src/test/java/de.htw.ai.kbe.songsrx.service/SongWebServiceTest.java
+++ songsrx/src/test/java/de.htw.ai.kbe.songsrx.service/SongWebServiceTest.java
@@ -1,195 +1,70 @@
-//package de.htw.ai.kbe.songsrx.service;
-//
-//import de.htw.ai.kbe.songsrx.bean.Song;
-//import javax.inject.Singleton;
-//import javax.ws.rs.client.Entity;
-//import javax.ws.rs.core.Application;
-//import javax.ws.rs.core.MediaType;
-//import javax.ws.rs.core.Response;
-//
-//import org.glassfish.hk2.utilities.binding.AbstractBinder;
-//import org.glassfish.jersey.server.ResourceConfig;
-//import org.glassfish.jersey.test.JerseyTest;
-//import org.junit.Assert;
-//import org.junit.Test;
-//
-//import javax.inject.Singleton;
-//import javax.ws.rs.core.Application;
-//
-//public class SongWebServiceTest extends JerseyTest {
-//
-//    @Override
-//    protected Application configure() {
-//        return new ResourceConfig(SongsResource.class).register(
-//                new AbstractBinder() {
-//                    @Override
-//                    protected void configure() {
-//                        bind(InMemorySongsPersistence.class).to(ISongPersistence.class).in(Singleton.class);
-//                    }
-//                });
-//    }
-//
-//    // -------------
-//    // get Tests
-//    // -------------
-//
-////    @Test
-////    public void getSongWithValidIdShouldReturnSong() {
-////        Song song = target("/songs/1").request(MediaType.APPLICATION_XML).get(Song.class);
-////        System.out.println(song);
-////        Assert.assertEquals(1, song.getId().intValue());
-////    }
-////
-////    @Test
-////    public void getSongWithStringIdShouldReturn404() {
-////        Response response = target("/songs/stringAndNoId").request().get();
-////        Assert.assertEquals(404, response.getStatus());
-////    }
-////
-////    @Test
-////    public void getSongWithNonExistingIdShouldReturn404(){
-////        Response response = target("/songs/999").request().get();
-////        Assert.assertEquals(404, response.getStatus());
-////    }
-//
-//    // -------------
-//    // put Tests
-//    // -------------
-//
-//    // JSON
-//    @Test
-//    public void updateSongJsonWithNonExistingIdShouldReturn404() {
-//        Song song = getTestSongWithoutId();
-//
-//        Response response = target("/songs/999").request().put(Entity.json(song));
-//        Assert.assertEquals(404, response.getStatus());
-//    }
-//
-//    // XML
-//    @Test
-//    public void updateSongXmlWithNonExistingIdShouldReturn404() {
-//        Song song = getTestSongWithoutId();
-//
-//        Response response = target("/songs/999").request().put(Entity.xml(song));
-//        Assert.assertEquals(404, response.getStatus());
-//    }
-//
-//    // JSON
-//    @Test
-//    public void updateSongJsonShouldReturn204AndUpdatedSong() {
-//        Song songPutTest = getTestSongWithIdOne();
-//
-//        // the original song
-//        Song songGetTest = target("/songs/1").request(MediaType.APPLICATION_JSON).get(Song.class);
-//        assertSong(songGetTest, 1, "Justin Timberlake", "Trolls", "Can’t Stop the Feeling", 2016);
-//
-//        Response response = target("/songs/1").request().put(Entity.json(songPutTest));
-//        Assert.assertEquals(204, response.getStatus());
-//
-//        // the new overwritten song
-//        songGetTest = target("/songs/1").request(MediaType.APPLICATION_JSON).get(Song.class);
-//        assertSong(songGetTest, 1, "Jimi Hendrix", "Axis: Bold As Love", "Little Wing", 1967);
-//    }
-//
-//    // XML
-//    @Test
-//    public void updateSongXmlShouldReturn204AndUpdatedSong() {
-//        Song songPutTest = getTestSongWithIdOne();
-//
-//        // the original song
-//        Song songGetTest = target("/songs/1").request(MediaType.APPLICATION_XML).get(Song.class);
-//        assertSong(songGetTest, 1, "Justin Timberlake", "Trolls", "Can’t Stop the Feeling", 2016);
-//
-//        Response response = target("/songs/1").request().put(Entity.xml(songPutTest));
-//        Assert.assertEquals(204, response.getStatus());
-//
-//        // the new overwritten song
-//        songGetTest = target("/songs/1").request(MediaType.APPLICATION_XML).get(Song.class);
-//        assertSong(songGetTest, 1, "Jimi Hendrix", "Axis: Bold As Love", "Little Wing", 1967);
-//    }
-//
-//    // JSON
-//    @Test
-//    public void updateSongJsonWithDifferentIdInBodyShouldReturn400() {
-//        Song song = getTestSongWithIdOne();
-//
-//        Response response = target("/songs/2").request().put(Entity.json(song));
-//        Assert.assertEquals(400, response.getStatus());
-//    }
-//
-//    // XML
-//    @Test
-//    public void updateSongXmlWithDifferentIdInBodyShouldReturn400() {
-//        Song song = getTestSongWithIdOne();
-//
-//        Response response = target("/songs/2").request().put(Entity.xml(song));
-//        Assert.assertEquals(400, response.getStatus());
-//    }
-//
-//    // JSON
-//    @Test
-//    public void updateSongJsonWithNoIdWithinPayloadShouldReturn204AndUpdatedSong() {
-//        Song songPutTest = getTestSongWithoutId();
-//
-//        // the original song
-//        Song songGetTest = target("/songs/1").request(MediaType.APPLICATION_XML).get(Song.class);
-//        assertSong(songGetTest, 1, "Justin Timberlake", "Trolls", "Can’t Stop the Feeling", 2016);
-//
-//        Response response = target("/songs/1").request().put(Entity.json(songPutTest));
-//        Assert.assertEquals(204, response.getStatus());
-//
-//        // the new overwritten song
-//        songGetTest = target("/songs/1").request(MediaType.APPLICATION_XML).get(Song.class);
-//        assertSong(songGetTest, 1, "Jimi Hendrix", "Axis: Bold As Love", "Little Wing", 1967);
-//    }
-//
-//    // XML
-//    @Test
-//    public void updateSongXmlWithNoIdWithinPayloadShouldReturn204AndUpdatedSong() {
-//        Song songPutTest = getTestSongWithoutId();
-//
-//        // the original song
-//        Song songGetTest = target("/songs/1").request(MediaType.APPLICATION_XML).get(Song.class);
-//        assertSong(songGetTest, 1, "Justin Timberlake", "Trolls", "Can’t Stop the Feeling", 2016);
-//
-//        Response response = target("/songs/1").request().put(Entity.xml(songPutTest));
-//        Assert.assertEquals(204, response.getStatus());
-//
-//        // the new overwritten song
-//        songGetTest = target("/songs/1").request(MediaType.APPLICATION_XML).get(Song.class);
-//        assertSong(songGetTest, 1, "Jimi Hendrix", "Axis: Bold As Love", "Little Wing", 1967);
-//    }
-//
-//    @Test
-//    public void updateSongWithWrongParamaterShouldReturn404() {
-//        Song songPutTest = getTestSongWithoutId();
-//
-//        Response response = target("/songs/garbage").request().put(Entity.xml(songPutTest));
-//        Assert.assertEquals(404, response.getStatus());
-//    }
-//
-//    @Test
-//    public void updateSongWithWrongPayloadShouldReturn400() {
-//        Song songPutTest = getTestSongWithWrongPayload();
-//
-//        Response response = target("/songs/1").request().put(Entity.xml(songPutTest));
-//        Assert.assertEquals(400, response.getStatus());
-//    }
-//
-//    @Test
-//    public void updateSongJsonJustWithTitleShouldReturn204AndUpdatedSongs() {
-//        Song songPutTest = getTestSongWithJustATitle();
-//
-//        // the original song
-//        Song songGetTest = target("/songs/1").request(MediaType.APPLICATION_JSON).get(Song.class);
-//        assertSong(songGetTest, 1, "Justin Timberlake", "Trolls", "Can’t Stop the Feeling", 2016);
-//
-//        Response response = target("/songs/1").request().put(Entity.xml(songPutTest));
-//        Assert.assertEquals(204, response.getStatus());
-//
-//        // the new overwritten song
-//        songGetTest = target("/songs/1").request(MediaType.APPLICATION_JSON).get(Song.class);
-//        assertSong(songGetTest, 1, null, null, "Supermix 2000", null);
-//    }
-//}
-//
+package de.htw.ai.kbe.songsrx.service;
+
+import de.htw.ai.kbe.songsrx.bean.Song;
+import de.htw.ai.kbe.songsrx.persistence.ISongList;
+import de.htw.ai.kbe.songsrx.persistence.InMemorySongList;
+import org.glassfish.hk2.utilities.binding.AbstractBinder;
+import org.glassfish.jersey.server.ResourceConfig;
+import org.glassfish.jersey.test.JerseyTest;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+import javax.inject.Singleton;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.core.Application;
+import javax.ws.rs.core.HttpHeaders;
+
+import javax.ws.rs.core.Response;
+
+public class SongWebServiceTest extends JerseyTest {
+
+    @Override
+    protected Application configure() {
+        return new ResourceConfig(SongWebService.class).register(new AbstractBinder() {
+
+            @Override
+            protected void configure() {
+                bind(InMemorySongList.class).to(ISongList.class).in(Singleton.class);
+
+            }
+        });
+    }
+
+    // Testcases für Put
+    @Test
+    public void putWithNonExistingIdXML() {
+        Song s = new Song.Builder("XML Song").artist("XML Artist").album("XML Album").released(2018).build();
+        Response response = target("/songs/1337").request().header(HttpHeaders.AUTHORIZATION, "mmuster").put(Entity.xml(s));
+        assertEquals(404, response.getStatus());
+    }
+
+    @Test
+    public void putWithNonExistingIdJSON() {
+        Song s = new Song.Builder("JSON Song").artist("JSON Artist").album("JSON Album").released(2018).build();
+        Response response = target("/songs/1337").request().header(HttpHeaders.AUTHORIZATION, "mmuster")
+                .put(Entity.json(s));
+        assertEquals(404, response.getStatus());
+    }
+
+    @Test
+    public void putWithExistingIdXML() {
+        Song s = new Song.Builder("XML Song").artist("XML Artist").album("XML Album").released(2018).id(1)
+                .build();
+
+        Response response = target("/songs/1").request().header(HttpHeaders.AUTHORIZATION, "mmuster").put(Entity.xml(s));
+        assertEquals(204, response.getStatus());
+    }
+
+    @Test
+    public void putWithExistingIdJSON() {
+        Song s = new Song.Builder("JSON Song").artist("JSON Artist").album("JSON Album").released(2018).id(1)
+                .build();
+
+        Response response = target("/songs/1").request().header(HttpHeaders.AUTHORIZATION, "mmuster").put(Entity.json(s));
+        assertEquals(204, response.getStatus());
+    }
+
+
+}
